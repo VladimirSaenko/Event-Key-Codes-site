1e353488f01b41db2f74b5cd3838639f07368fda
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-05 17:52:40 +0300

    Fix bug

--- mainscript.js
+++ mainscript.js
@@ -42,19 +42,19 @@ window.addEventListener('keydown', (event) => {
     }
 
     else if(event.key === 'w' || event.key === 'W') {
-        body.style.background = 'url(/backgrounds/w.jpg)';
+        body.style.background = 'url(backgrounds/w.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.key === 'e' || event.key === 'E') {
-        body.style.background = 'url(/backgrounds/e.jpg)';
+        body.style.background = 'url(backgrounds/e.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.key === 't' || event.key === 'T') {
-        body.style.background = 'url(/backgrounds/t.jpg)';
+        body.style.background = 'url(backgrounds/t.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
@@ -64,7 +64,7 @@ window.addEventListener('keydown', (event) => {
     }
 
     else if(event.key === 'o' || event.key === 'O') {
-        body.style.background = 'url(/backgrounds/o.jpeg)';
+        body.style.background = 'url(backgrounds/o.jpeg)';
     }
 
     else if(event.key === 'p' || event.key === 'P') {
@@ -73,18 +73,18 @@ window.addEventListener('keydown', (event) => {
 
     else if(event.key === '[' || event.key === ']' || event.key === '|' || event.key === ';'
     || event.key === '/' || event.key === ',' || event.key === '.') {
-        body.style.background = 'url(/backgrounds/skobka.jpg)';
+        body.style.background = 'url(backgrounds/skobka.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
         body.style.backgroundRepeat = 'no-repeat';
     }
 
     else if(event.key === 'a' || event.key === 'A') {
-        body.style.background = 'url(/backgrounds/a.jpg)';
+        body.style.background = 'url(backgrounds/a.jpg)';
     }
 
     else if(event.key === 's' || event.key === 'S') {
-        body.style.background = 'url(/backgrounds/s.jpg)';
+        body.style.background = 'url(backgrounds/s.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundRepeat = 'no-repeat';
     }
@@ -98,44 +98,44 @@ window.addEventListener('keydown', (event) => {
     }
 
     else if(event.key === 'f' || event.key === 'F') {
-        body.style.background = 'url(/backgrounds/f.jpg)';
+        body.style.background = 'url(backgrounds/f.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.key === 'h' || event.key === 'H') {
-        body.style.background = 'url(/backgrounds/h.png)';
+        body.style.background = 'url(backgrounds/h.png)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
         body.style.backgroundRepeat = 'no-repeat';
     }
 
     else if(event.key === 'v' || event.key === 'V') {
-        body.style.background = 'url(/backgrounds/v.jpg)';
+        body.style.background = 'url(backgrounds/v.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.key === 'x' || event.key === 'X') {
-        body.style.background = 'url(/backgrounds/x.png)';
+        body.style.background = 'url(backgrounds/x.png)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'auto';
     }
 
     else if(event.keyCode === 16 || event.keyCode === 18 || event.keyCode === 17 || event.keyCode === 46) {
-        body.style.background = 'url(/backgrounds/bs.jpg)';
+        body.style.background = 'url(backgrounds/bs.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.keyCode === 13) {
-        body.style.background = 'url(/backgrounds/enter.jpg)';
+        body.style.background = 'url(backgrounds/enter.jpg)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
 
     else if(event.keyCode === 91) {
-        body.style.background = 'url(/backgrounds/meta.png)';
+        body.style.background = 'url(backgrounds/meta.png)';
         body.style.backgroundPosition = 'center';
         body.style.backgroundSize = 'cover';
     }
